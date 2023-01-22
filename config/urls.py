10f30ea5ffe3be8d2c435b9f6c755a5fc6292690
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-01-22 17:56:47 -0500

    'navbar' operational, will need to move user admin links in the future.

--- config/urls.py
+++ config/urls.py
@@ -16,19 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-
-from config.settings.common import THE_SITE_NAME
+from django.views.generic.base import TemplateView, RedirectView
 
 urlpatterns = [
-    path(
-        '',
-        TemplateView.as_view(
-            template_name="home.html",
-            extra_context={'the_site_name': THE_SITE_NAME},
-        ),
-        name='home',
-    ),
+    path('', RedirectView.as_view(pattern_name='projects:index')),
     path(
         'temp-base/',   # http://localhost:8000/temp-base/
         TemplateView.as_view(
